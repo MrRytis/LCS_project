92dce22080c34f58313e3bd723e905bd5606b249
daiarn <dainius1997@gmail.com>
2018-12-05 22:11:44 +0200

    Added requests for all functions

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,8 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\AtaskaitosTipai;
+use AppBundle\Entity\Matmenys;
+use AppBundle\Entity\SiuntimoBudai;
+use AppBundle\Entity\Uzpildai;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends AbstractController
 {
@@ -15,26 +20,37 @@ class AdminController extends AbstractController
     }
     public function dimensionsAction()
     {
+        $dimensions = $this->getDoctrine()->getRepository(Matmenys::class)->findAll();
+//        dump($dimensions);
+//        die();
         return $this->render('Admin/dimensions.html.twig', [
             'controller_name' => 'AdminController',
+            'requests' => $dimensions,
         ]);
     }
     public function fillerAction()
     {
+        $filters = $this->getDoctrine()->getRepository(Uzpildai::class)->findAll();
         return $this->render('Admin/filler.html.twig', [
             'controller_name' => 'AdminController',
+            'requests' => $filters,
         ]);
     }
     public function shipping_methodsAction()
     {
+        $methods = $this->getDoctrine()->getRepository(SiuntimoBudai::class)->findAll();
         return $this->render('Admin/shipping_methods.html.twig', [
             'controller_name' => 'AdminController',
+            'requests' => $methods,
         ]);
     }
     public function reportAction()
     {
+        $reports = $this->getDoctrine()->getRepository(AtaskaitosTipai::class)->findAll();
+
         return $this->render('Admin/report.html.twig', [
             'controller_name' => 'AdminController',
+            'requests' => $reports,
         ]);
     }
 }
