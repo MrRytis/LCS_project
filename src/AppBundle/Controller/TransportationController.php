a662fc21a5c364e19629ff5ed4bdc2ac01de13ea
daiarn <dainius1997@gmail.com>
2018-12-06 04:51:07 +0200

    Added logic into transportation controller

--- src/AppBundle/Controller/TransportationController.php
+++ src/AppBundle/Controller/TransportationController.php
@@ -3,14 +3,24 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use AppBundle\Entity\Uzpildai;
+use AppBundle\Entity\Matmenys;
+use AppBundle\Entity\SiuntimoBudai;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TransportationController extends AbstractController
 {
     public function indexAction()
     {
+        $dimensions = $this->getDoctrine()->getRepository(Matmenys::class)->findAll();
+        $filters = $this->getDoctrine()->getRepository(Uzpildai::class)->findAll();
+        $methods = $this->getDoctrine()->getRepository(SiuntimoBudai::class)->findAll();
+
         return $this->render('Order/transportation.html.twig', [
             'controller_name' => 'TransportationController',
+            'dimensions' => $dimensions,
+            'filters' => $filters,
+            'methods' => $methods,
         ]);
     }
 }
